f06467e3dae6de4aebce927433e60f82acaf764f
harry-duncan <harryduncan25@gmail.com>
2015-11-26 17:40:08 +1100

    html layout almost complete

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
+
+/////////// Game functions //////////////
+
 // I need a deal function 
 
 // I need a hit function 
 
-// I need a stay function 
+// I need a sit function 
+
+// i need a new game function 
+
+// I need a shuffle function 
+
+/////////// data shit ///////////////////
 
 // I need a way to store card data 
 
 // I need a way to be able to store hand data
 
-// I need a shuffle function 
-
 // I need a score function 
+
+// i need a way to store the score and display it 
 
 // I need a way to update the score on screen 
 
 // I need a score board 
+
+// need a player account to add money too // reference the bank functions you wrote 
+
+
+
+
+
+// Optional shit //
+
+// could add a theme button so you can change themes and such 
+
+// could be able to set a player photo and name 
+
+// could have the dealer say random shit to you like 'good fucking luck' and other abusive shit'
+
+
 
 
 
