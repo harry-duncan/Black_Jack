bf1dcbc3125a9c5a33d6e3515b86495011694aa0
harry-duncan <harryduncan25@gmail.com>
2015-11-26 14:47:21 +1100

    making sure shit is up to date

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// I need a deal function 
+
+// I need a hit function 
+
+// I need a stay function 
+
+// I need a way to store card data 
+
+// I need a way to be able to store hand data
+
+// I need a shuffle function 
+
+// I need a score function 
+
+// I need a way to update the score on screen 
+
+// I need a score board 
+
+
+
+
+
